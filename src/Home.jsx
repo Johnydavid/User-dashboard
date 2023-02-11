22a3e9df20239bcca407059ec3a9da30e81fab0c
Johnson David <92262280+Johnydavid@users.noreply.github.com>
2023-02-11 13:53:34 +0530

    changes in home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import "./App.css";
 import Typewriter from "typewriter-effect";
@@ -25,31 +25,3 @@ export default function Home() {
     </div>
   );
 }
-=======
-import React from "react";
-import "./App.css";
-import Typewriter from "typewriter-effect";
-
-export default function Home() {
-  return (
-    <div className="container-fluid">
-      <h1 className="crudText">CRUD Operation</h1>
-
-      <div className="homeText">
-        <Typewriter
-          options={{
-            strings: [
-              "C ---> Create",
-              "R ---> Read",
-              "U ---> Update",
-              "D ---> Delete",
-            ],
-            autoStart: true,
-            loop: true,
-          }}
-        />
-      </div>
-    </div>
-  );
-}
->>>>>>> dd83b65f508b2cff4a4bf75f2fec9ea56159271d
